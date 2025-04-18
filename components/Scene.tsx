ecc0d59a7f4b3f8cc8dfa0cd776c0eef65115cb5
Yen-Chun Lin <william.lin@introvista.ai>
2025-04-18 18:50:39 +0800

    chore: add OrbitControls

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -2,14 +2,15 @@
 
 import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
-import { Environment } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 export default function Index() {
   return (
-    <Canvas style={{ background: "#000000" }}>
+    <Canvas style={{ background: "#000" }}>
       <Model />
       <directionalLight intensity={2} position={[0, 2, 3]} />
       <Environment preset="city" />
+      <OrbitControls enablePan={false} maxDistance={15} />
     </Canvas>
   );
 }
